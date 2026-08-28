4772a76c9f02d7a684720fc28d6946fb297e299b
ChambersXDU <78330120+ChambersXDU@users.noreply.github.com>
2026-08-28 23:56:27 +0800

    Add unit test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,4 +36,6 @@ dependencies {
     implementation(composeBom)
     implementation("androidx.activity:activity-compose:1.13.0")
     implementation("androidx.compose.material3:material3")
+
+    testImplementation("junit:junit:4.13.2")
 }
