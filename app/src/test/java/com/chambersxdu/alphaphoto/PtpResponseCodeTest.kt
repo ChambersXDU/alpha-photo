6a0071d8a00eaada5d93353da0bf422ccb547b7f
ChambersXDU <78330120+ChambersXDU@users.noreply.github.com>
2026-08-29 09:45:36 +0800

    Test PTP response descriptions

---
+++ app/src/test/java/com/chambersxdu/alphaphoto/PtpResponseCodeTest.kt
@@ -0,0 +1,20 @@
+package com.chambersxdu.alphaphoto
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class PtpResponseCodeTest {
+    @Test
+    fun describesKnownStandardAndSonyCodes() {
+        assertEquals("OK", PtpResponseCodes.describe(0x2001))
+        assertEquals("Invalid Storage ID", PtpResponseCodes.describe(0x2008))
+        assertEquals("Invalid Object Handle", PtpResponseCodes.describe(0x2009))
+        assertEquals("Device Busy", PtpResponseCodes.describe(0x2019))
+        assertEquals("Camera Status Error", PtpResponseCodes.describe(0xA106))
+    }
+
+    @Test
+    fun formatsUnknownCodesWithoutLosingTheValue() {
+        assertEquals("Unknown response 0x2abc", PtpResponseCodes.describe(0x2ABC))
+    }
+}
